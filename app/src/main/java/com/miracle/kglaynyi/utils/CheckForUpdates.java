2c2595fce8457f7e9f361c3b7f91dfb738781f30
kglaynyi <94588643+kglaynyi@users.noreply.github.com>
2026-08-26 16:28:40 +0630

    Harden update checker and use Miracle releases

--- app/src/main/java/com/miracle/kglaynyi/utils/CheckForUpdates.java
+++ app/src/main/java/com/miracle/kglaynyi/utils/CheckForUpdates.java
@@ -14,78 +14,79 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class CheckForUpdates extends BaseFragment {
+    private static final String TAG = "CheckForUpdates";
+    private static final String RELEASES_URL =
+            "https://api.github.com/repos/kglaynyi/Miracle/releases?per_page=1";
+
     public GitHubResponse[] checkForUpdates() throws IOException {
-        String GET_URL = "https://api.github.com/repos/anujd64/Thunder/releases";
-        int versionCode = BuildConfig.VERSION_CODE;
-        String versionName = BuildConfig.VERSION_NAME;
-        GitHubResponse[] gitHubResponse;
-        StringBuffer response = null;
-        URL obj = new URL(GET_URL);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("GET");
-        int responseCode = con.getResponseCode();
-        System.out.println("GET Response Code :: " + responseCode);
-        if (responseCode == HttpURLConnection.HTTP_OK) { // success
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    con.getInputStream()));
-            String inputLine;
-            response = new StringBuffer();
+        HttpURLConnection con = null;
+        try {
+            URL obj = new URL(RELEASES_URL);
+            con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setConnectTimeout(10000);
+            con.setReadTimeout(10000);
+            con.setRequestProperty("Accept", "application/vnd.github+json");
 
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
+            int responseCode = con.getResponseCode();
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                Log.w(TAG, "GitHub releases request returned HTTP " + responseCode);
+                return null;
             }
-            in.close();
 
-            Gson gson = new Gson();
-            gitHubResponse = gson.fromJson(response.toString() ,GitHubResponse[].class);
-            String gitHubVersion = gitHubResponse[0].tag_name.substring(1);
-            Log.i("Check fOr Update",gitHubVersion + "current" + versionName );
-
-                if(versionCompare(gitHubVersion,versionName)==1){
-                    /**UpdateFragment*/
-                    return gitHubResponse;
+            StringBuilder response = new StringBuilder();
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
                 }
+            }
 
-        } else {
-            System.out.println("GET request did not work");
+            GitHubResponse[] releases = new Gson().fromJson(response.toString(), GitHubResponse[].class);
+            if (releases == null || releases.length == 0 || releases[0] == null || releases[0].tag_name == null) {
+                return null;
+            }
+
+            String tagName = releases[0].tag_name.trim();
+            String githubVersion = tagName.startsWith("v") ? tagName.substring(1) : tagName;
+            String currentVersion = BuildConfig.VERSION_NAME;
+            Log.i(TAG, "latest=" + githubVersion + ", current=" + currentVersion);
+
+            return versionCompare(githubVersion, currentVersion) == 1 ? releases : null;
+        } catch (RuntimeException e) {
+            Log.e(TAG, "Unable to parse update information", e);
+            return null;
+        } finally {
+            if (con != null) {
+                con.disconnect();
+            }
         }
-
-        assert response != null;
-        Log.i("Check fOr Update",response.toString());
-
-    return null;
-
     }
 
-    static int versionCompare(String v1, String v2)
-    {
-
+    static int versionCompare(String v1, String v2) {
         int vnum1 = 0, vnum2 = 0;
 
-        for (int i = 0, j = 0; (i < v1.length()
-                || j < v2.length());) {
-            while (i < v1.length()
-                    && v1.charAt(i) != '.') {
-                vnum1 = vnum1 * 10
-                        + (v1.charAt(i) - '0');
+        for (int i = 0, j = 0; (i < v1.length() || j < v2.length());) {
+            while (i < v1.length() && v1.charAt(i) != '.') {
+                char c = v1.charAt(i);
+                if (!Character.isDigit(c)) return 0;
+                vnum1 = vnum1 * 10 + (c - '0');
                 i++;
             }
-            while (j < v2.length()
-                    && v2.charAt(j) != '.') {
-                vnum2 = vnum2 * 10
-                        + (v2.charAt(j) - '0');
+            while (j < v2.length() && v2.charAt(j) != '.') {
+                char c = v2.charAt(j);
+                if (!Character.isDigit(c)) return 0;
+                vnum2 = vnum2 * 10 + (c - '0');
                 j++;
             }
 
-            if (vnum1 > vnum2)
-                return 1;
-            if (vnum2 > vnum1)
-                return -1;
+            if (vnum1 > vnum2) return 1;
+            if (vnum2 > vnum1) return -1;
+
             vnum1 = vnum2 = 0;
             i++;
             j++;
         }
         return 0;
     }
-
 }
