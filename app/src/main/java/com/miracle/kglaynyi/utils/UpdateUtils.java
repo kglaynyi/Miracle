73d3424c7b920b55b8ef37dd8921759558db4dac
kglaynyi <94588643+kglaynyi@users.noreply.github.com>
2026-08-26 16:28:51 +0630

    Make update UI handling crash-safe

--- app/src/main/java/com/miracle/kglaynyi/utils/UpdateUtils.java
+++ app/src/main/java/com/miracle/kglaynyi/utils/UpdateUtils.java
@@ -1,30 +1,44 @@
 package com.miracle.kglaynyi.utils;
 
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.miracle.kglaynyi.R;
 import com.miracle.kglaynyi.fragments.UpdateAppFragment;
 import com.miracle.kglaynyi.model.GitHubResponse;
 
-import java.io.IOException;
 import java.util.concurrent.Executors;
 
 public class UpdateUtils {
-    public static void checkForUpdates(AppCompatActivity appCompatActivity){
-        Executors.newSingleThreadExecutor().execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    GitHubResponse[] gitHubResponses = new CheckForUpdates().checkForUpdates();
-                    if (gitHubResponses != null) {
-                        UpdateAppFragment updateAppFragment = new UpdateAppFragment(gitHubResponses);
-                        appCompatActivity.getSupportFragmentManager().beginTransaction().add(R.id.container , updateAppFragment).addToBackStack(null).commit();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+    private static final String TAG = "UpdateUtils";
+
+    public static void checkForUpdates(AppCompatActivity appCompatActivity) {
+        Executors.newSingleThreadExecutor().execute(() -> {
+            try {
+                GitHubResponse[] gitHubResponses = new CheckForUpdates().checkForUpdates();
+                if (gitHubResponses == null || gitHubResponses.length == 0) {
+                    return;
                 }
+
+                appCompatActivity.runOnUiThread(() -> {
+                    if (appCompatActivity.isFinishing() || appCompatActivity.isDestroyed()) {
+                        return;
+                    }
+                    try {
+                        UpdateAppFragment updateAppFragment = new UpdateAppFragment(gitHubResponses);
+                        appCompatActivity.getSupportFragmentManager()
+                                .beginTransaction()
+                                .add(R.id.container, updateAppFragment)
+                                .addToBackStack(null)
+                                .commitAllowingStateLoss();
+                    } catch (RuntimeException e) {
+                        Log.e(TAG, "Unable to show update dialog", e);
+                    }
+                });
+            } catch (Exception e) {
+                Log.w(TAG, "Update check failed; continuing without update prompt", e);
             }
         });
     }
-
 }
