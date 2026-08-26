0017580361a748dde7a05833e98debb71408244e
kglaynyi <94588643+kglaynyi@users.noreply.github.com>
2026-08-26 17:07:49 +0630

    Validate indexes before saving and report scan results

--- app/src/main/java/com/miracle/kglaynyi/fragments/AddNewIndexFragment.java
+++ app/src/main/java/com/miracle/kglaynyi/fragments/AddNewIndexFragment.java
@@ -1,9 +1,11 @@
 package com.miracle.kglaynyi.fragments;
 
+import static com.miracle.kglaynyi.utils.SendPostRequest.getScannedVideoCount;
 import static com.miracle.kglaynyi.utils.SendPostRequest.postRequestGDIndex;
 import static com.miracle.kglaynyi.utils.SendPostRequest.postRequestGoIndex;
 import static com.miracle.kglaynyi.utils.SendPostRequest.postRequestMapleIndex;
 import static com.miracle.kglaynyi.utils.SendPostRequest.postRequestSimpleProgramIndex;
+import static com.miracle.kglaynyi.utils.SendPostRequest.resetPagingState;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -22,7 +24,7 @@ import androidx.annotation.Nullable;
 import com.miracle.kglaynyi.R;
 import com.miracle.kglaynyi.database.DatabaseClient;
 import com.miracle.kglaynyi.model.IndexLink;
-
+import com.miracle.kglaynyi.utils.IndexConnectionValidator;
 
 public class AddNewIndexFragment extends BaseFragment {
 
@@ -30,199 +32,135 @@ public class AddNewIndexFragment extends BaseFragment {
     private EditText userNameView;
     private EditText passWordView;
     private Button save;
-    private ProgressBar progress_circular;
-    private TextView refreshSuggest;
-
-//    private MaterialButtonToggleGroup toggleIndexTypeGroup;
-//    private MaterialButton toggleIndexTypeButton;
-//
-//    private MaterialButtonToggleGroup toggleFolderTypeGroup;
-//    private MaterialButton toggleFolderTypeButton;
-
-
-    private AutoCompleteTextView actv1;
-    private AutoCompleteTextView actv2;
-
-//    private RadioGroup radioIndexTypeGroup;
-//    private RadioButton radioIndexTypeButton;
-//
-//    private RadioGroup radioFolderTypeGroup;
-//    private RadioButton radioFolderTypeButton;
-
+    private ProgressBar progressCircular;
+    private TextView statusText;
+    private AutoCompleteTextView indexTypeView;
+    private AutoCompleteTextView folderTypeView;
 
     @Nullable
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.fragment_add_index, container, false);
     }
 
-
+    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        indexLinkView =view.findViewById(R.id.indexlink);
+        indexLinkView = view.findViewById(R.id.indexlink);
         userNameView = view.findViewById(R.id.username);
         passWordView = view.findViewById(R.id.password);
         save = view.findViewById(R.id.save);
-        progress_circular = view.findViewById(R.id.progress_circular);
-        refreshSuggest = view.findViewById(R.id.suggestRefresh);
+        progressCircular = view.findViewById(R.id.progress_circular);
+        statusText = view.findViewById(R.id.suggestRefresh);
+        indexTypeView = view.findViewById(R.id.actv);
+        folderTypeView = view.findViewById(R.id.actv2);
 
-//        toggleIndexTypeGroup = view.findViewById(R.id.index_type_group);
-//        toggleFolderTypeGroup = view.findViewById(R.id.folder_type_group);
+        String[] indexTypes = mActivity.getResources().getStringArray(R.array.index_types);
+        indexTypeView.setAdapter(new ArrayAdapter<>(mActivity, R.layout.item_index_type, indexTypes));
 
+        String[] folderTypes = mActivity.getResources().getStringArray(R.array.folder_types);
+        folderTypeView.setAdapter(new ArrayAdapter<>(mActivity, R.layout.item_folder_type, folderTypes));
 
-        actv1 = view.findViewById(R.id.actv);
-        actv2 = view.findViewById(R.id.actv2);
-
-        String[] index_types = mActivity.getResources().getStringArray(R.array.index_types);
-        ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<>(mActivity , R.layout.item_index_type , index_types);
-        actv1.setAdapter(arrayAdapter1);
-
-
-        String[] folder_types = mActivity.getResources().getStringArray(R.array.folder_types);
-        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<>(mActivity , R.layout.item_folder_type , folder_types);
-        actv2.setAdapter(arrayAdapter2);
-
-
-
-
-
-//        radioIndexTypeGroup=(RadioGroup)view.findViewById(R.id.rb_group);
-//        radioFolderTypeGroup=(RadioGroup)view.findViewById(R.id.folderTypeRadioGroup);
-
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                IndexLink indexLink = new IndexLink();
-                if(indexLinkView.getText().toString().equals("")){
-                    indexLinkView.setError("Enter index link");
-                }else{
-                    indexLink.setLink(indexLinkView.getText().toString());
-                }
-                indexLink.setUsername(userNameView.getText().toString());
-                indexLink.setPassword(passWordView.getText().toString());
-                indexLink.setIndexType(actv1.getText().toString());
-                indexLink.setFolderType(actv2.getText().toString());
-
-
-
-//                int IndexTypeCheckedButtonId = toggleIndexTypeGroup.getCheckedButtonId();
-//                toggleIndexTypeButton  = view.findViewById(IndexTypeCheckedButtonId);
-//                int FolderTypeCheckedButtonId = toggleFolderTypeGroup.getCheckedButtonId();
-//                toggleFolderTypeButton = view.findViewById(FolderTypeCheckedButtonId);
-
-//                int indexTypeButtonSelectedId=radioIndexTypeGroup.getCheckedRadioButtonId();
-//                int folderTypeButtonSelectedId=radioFolderTypeGroup.getCheckedRadioButtonId();
-//                radioIndexTypeButton= mActivity.findViewById(indexTypeButtonSelectedId);
-//                radioFolderTypeButton= mActivity.findViewById(folderTypeButtonSelectedId);
-
-
-                try{
-                    if(indexLink.getLink().length()<1){
-                       indexLinkView.setError("Invalid Link");
-                    }
-//                    if(toggleIndexTypeButton == null || toggleFolderTypeButton == null){
-//                    refreshSuggest.setVisibility(View.VISIBLE);
-//                    refreshSuggest.setText("Select Index and Folder Type");
-//                    }
-//                    if(toggleIndexTypeButton != null || toggleFolderTypeButton != null && indexLink.getUsername().length()<1 && indexLink.getPassword().length()<1){
-//                            userNameView.setVisibility(View.GONE);
-//                            passWordView.setVisibility(View.GONE);
-//                        indexLink.setIndexType(toggleIndexTypeButton.getText().toString());
-//                        indexLink.setFolderType(toggleFolderTypeButton.getText().toString());
-//                    }
-
-
-                    Thread thread = new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            if(DatabaseClient.getInstance(mActivity).getAppDatabase().indexLinksDao().find(indexLink.getLink())!=null){
-                                //refresh instead
-                                mActivity.runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        refreshSuggest.setVisibility(View.VISIBLE);
-                                    }
-                                });
-                            }
-                            else if(indexLink.getLink()!=null) { DatabaseClient.getInstance(mActivity).getAppDatabase().indexLinksDao().insert(indexLink);
-                                mActivity.runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        save.setText("Adding");
-                                        progress_circular.setVisibility(View.VISIBLE);
-                                    }
-                                });
-
-
-
-                                String folderType = indexLink.getFolderType();
-                                String indexType = indexLink.getIndexType();
-
-                                String link =indexLink.getLink();
-                                String user =indexLink.getUsername();
-                                String pass =indexLink.getPassword();
-
-                                System.out.println("Before setting id"+ indexLink.getId());
-
-                                IndexLink indexLinkAgain = DatabaseClient.getInstance(mActivity)
-                                        .getAppDatabase()
-                                        .indexLinksDao()
-                                        .find(link);
-
-                                int index_id = indexLinkAgain.getId();
-
-                                System.out.println("After setting id"+ indexLinkAgain.getId());
-
-                                if(folderType.equals("Movies")) {
-                                    if(indexType.equals("GDIndex")) {
-                                        postRequestGDIndex(link,user,pass,false,index_id);
-                                    }
-                                    if(indexType.equals("GoIndex")) {
-                                        postRequestGoIndex(link,user,pass,false,index_id);
-                                    }
-                                    if(indexType.equals("Maple")){
-                                        postRequestMapleIndex(link,user,pass,false,index_id);
-                                    }
-                                    if(indexType.equals("SimpleProgram")){
-                                        postRequestSimpleProgramIndex(link,user,pass,false,index_id);
-                                    }
-                                }
-
-                                if(folderType.equals("TVShows")){
-                                    if(indexType.equals("GDIndex")) {
-                                        postRequestGDIndex(link,user,pass,true,index_id);
-                                    }
-                                    if(indexType.equals("GoIndex")) {
-                                        postRequestGoIndex(link,user,pass,true,index_id);
-                                    }
-                                    if(indexType.equals("MapleIndex")){
-                                        postRequestMapleIndex(link,user,pass,true,index_id);
-                                    }
-                                    if(indexType.equals("SimpleProgram")){
-                                        postRequestSimpleProgramIndex(link,user,pass,true,index_id);
-                                    }
-                                }
-                                mActivity.runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        save.setText("Done");
-                                        progress_circular.setVisibility(View.GONE);
-                                    }
-                                });
-                            }
-                        }
-                    });
-                    thread.start();
-                } catch (NullPointerException e) {
-                    e.printStackTrace();
-                }
-
-                if(save.getText().equals("Done")){
-                        mActivity.getSupportFragmentManager().popBackStack();
-                }
-            }
-        });
+        save.setOnClickListener(v -> addIndex());
     }
 
+    private void addIndex() {
+        String link = indexLinkView.getText().toString().trim();
+        String user = userNameView.getText().toString();
+        String pass = passWordView.getText().toString();
+        String indexType = indexTypeView.getText().toString().trim();
+        String folderType = folderTypeView.getText().toString().trim();
+
+        if (link.isEmpty()) {
+            indexLinkView.setError("Enter index link");
+            return;
+        }
+        if (!link.startsWith("http://") && !link.startsWith("https://")) {
+            indexLinkView.setError("URL must start with http:// or https://");
+            return;
+        }
+        if (indexType.isEmpty()) {
+            indexTypeView.setError("Select index type");
+            return;
+        }
+        if (folderType.isEmpty()) {
+            folderTypeView.setError("Select Movies or TVShows");
+            return;
+        }
+
+        setBusy(true, "Checking URL and credentials…");
+
+        new Thread(() -> {
+            IndexConnectionValidator.ValidationResult validation =
+                    IndexConnectionValidator.validate(link, user, pass, indexType);
+
+            if (!validation.success) {
+                mActivity.runOnUiThread(() -> setBusy(false, validation.message));
+                return;
+            }
+
+            try {
+                if (DatabaseClient.getInstance(mActivity).getAppDatabase().indexLinksDao().find(link) != null) {
+                    mActivity.runOnUiThread(() -> setBusy(false,
+                            "This index is already added. Use Manage Indexes to refresh it."));
+                    return;
+                }
+
+                IndexLink indexLink = new IndexLink();
+                indexLink.setLink(link);
+                indexLink.setUsername(user);
+                indexLink.setPassword(pass);
+                indexLink.setIndexType(indexType);
+                indexLink.setFolderType(folderType);
+
+                DatabaseClient.getInstance(mActivity).getAppDatabase().indexLinksDao().insert(indexLink);
+                IndexLink saved = DatabaseClient.getInstance(mActivity).getAppDatabase().indexLinksDao().find(link);
+                if (saved == null) {
+                    mActivity.runOnUiThread(() -> setBusy(false, "Could not save index"));
+                    return;
+                }
+
+                mActivity.runOnUiThread(() -> setBusy(true, "Index verified. Scanning videos…"));
+
+                resetPagingState();
+                boolean tvShows = "TVShows".equals(folderType);
+                int indexId = saved.getId();
+
+                if ("GDIndex".equals(indexType)) {
+                    postRequestGDIndex(link, user, pass, tvShows, indexId);
+                } else if ("GoIndex".equals(indexType)) {
+                    postRequestGoIndex(link, user, pass, tvShows, indexId);
+                } else if ("MapleIndex".equals(indexType) || "Maple".equals(indexType)) {
+                    postRequestMapleIndex(link, user, pass, tvShows, indexId);
+                } else if ("SimpleProgram".equals(indexType)) {
+                    postRequestSimpleProgramIndex(link, user, pass, tvShows, indexId);
+                }
+
+                int found = getScannedVideoCount();
+                String result = found > 0
+                        ? "Done. Found " + found + " video" + (found == 1 ? "." : "s.")
+                        : "Connected successfully, but no supported video files were found.";
+                mActivity.runOnUiThread(() -> {
+                    setBusy(false, result);
+                    save.setText("Done");
+                });
+            } catch (Exception e) {
+                String message = e.getMessage();
+                if (message == null || message.trim().isEmpty()) message = e.getClass().getSimpleName();
+                String finalMessage = "Index scan failed: " + message;
+                mActivity.runOnUiThread(() -> setBusy(false, finalMessage));
+            }
+        }).start();
+    }
+
+    private void setBusy(boolean busy, String message) {
+        save.setEnabled(!busy);
+        if (busy) save.setText("Working…");
+        else if (!"Done".contentEquals(save.getText())) save.setText("Save");
+        progressCircular.setVisibility(busy ? View.VISIBLE : View.GONE);
+        statusText.setVisibility(View.VISIBLE);
+        statusText.setText(message);
+    }
 }
