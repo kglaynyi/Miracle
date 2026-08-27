def412a2288a1261ccff3f716a8cc7cb20cb1694
kglaynyi <94588643+kglaynyi@users.noreply.github.com>
2026-08-27 20:21:15 +0630

    Add safe manual migration from database 30 to 31

--- app/src/main/java/com/miracle/kglaynyi/database/DatabaseClient.java
+++ app/src/main/java/com/miracle/kglaynyi/database/DatabaseClient.java
@@ -8,6 +8,13 @@ import androidx.sqlite.db.SupportSQLiteDatabase;
 
 public class DatabaseClient {
 
+    private static final Migration MIGRATION_30_31 = new Migration(30, 31) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE IndexLink ADD COLUMN selectedFoldersJson TEXT");
+        }
+    };
+
     private static final Migration MIGRATION_31_32 = new Migration(31, 32) {
         @Override
         public void migrate(SupportSQLiteDatabase database) {
@@ -28,7 +35,7 @@ public class DatabaseClient {
         //creating the app database with Room database builder
         //MyToDos is the name of the database
         appDatabase = Room.databaseBuilder(mCtx, AppDatabase.class, "MyToDos")
-                .addMigrations(MIGRATION_31_32)
+                .addMigrations(MIGRATION_30_31, MIGRATION_31_32)
                 .build();
     }
 
