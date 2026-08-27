c6513d6d8b8e4f6e8d7e7772122aff5a08bf8afa
kglaynyi <94588643+kglaynyi@users.noreply.github.com>
2026-08-27 14:54:52 +0630

    Grant Drive URI access for external movie playback

--- app/src/main/java/com/miracle/kglaynyi/fragments/MovieDetailsFragment.java
+++ app/src/main/java/com/miracle/kglaynyi/fragments/MovieDetailsFragment.java
@@ -410,6 +410,7 @@ public class MovieDetailsFragment extends BaseFragment{
                     addToLastPlayed();
                     Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(largestFile.getUrlString()));
                     intent.setDataAndType(Uri.parse(largestFile.getUrlString()), "video/*");
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                     startActivity(intent);
                 }
             });
