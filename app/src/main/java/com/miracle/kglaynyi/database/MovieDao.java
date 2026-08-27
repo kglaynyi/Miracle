fac061f570ef9b4f9efe9b81fe9a79e72f1bf997
kglaynyi <94588643+kglaynyi@users.noreply.github.com>
2026-08-27 19:56:12 +0630

    Track and clean movie sources by selected folder

--- app/src/main/java/com/miracle/kglaynyi/database/MovieDao.java
+++ app/src/main/java/com/miracle/kglaynyi/database/MovieDao.java
@@ -102,7 +102,13 @@ public interface MovieDao {
     @Query("DELETE FROM Movie WHERE index_id=:indexId AND fileName=:fileName AND size=:size AND gd_id=:stableId AND fileidForDB != (SELECT MAX(fileidForDB) FROM Movie WHERE index_id=:indexId AND fileName=:fileName AND size=:size AND gd_id=:stableId)")
     int deleteRepeatedStableSource(int indexId, String fileName, String size, String stableId);
 
-    @Query("UPDATE Movie SET urlString=:url, fileName=:fileName, size=:size, mimeType=:mimeType, modifiedTime=:modifiedTime WHERE gd_id=:stableId")
-    void updateSourceMetadata(String stableId, String url, String fileName, String size, String mimeType, Date modifiedTime);
+    @Query("UPDATE Movie SET urlString=:url, fileName=:fileName, size=:size, mimeType=:mimeType, modifiedTime=:modifiedTime, folder_path=:folderPath WHERE gd_id=:stableId")
+    void updateSourceMetadata(String stableId, String url, String fileName, String size, String mimeType, Date modifiedTime, String folderPath);
+
+    @Query("DELETE FROM Movie WHERE index_id=:indexId AND (folder_path=:folderPath OR folder_path LIKE :folderPrefix)")
+    int deleteByFolderPrefix(int indexId, String folderPath, String folderPrefix);
+
+    @Query("SELECT * FROM Movie WHERE index_id=:indexId AND (folder_path=:folderPath OR folder_path LIKE :folderPrefix)")
+    List<Movie> getByFolderPrefix(int indexId, String folderPath, String folderPrefix);
 
 }
